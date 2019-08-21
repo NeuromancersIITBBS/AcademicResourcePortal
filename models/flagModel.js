777870a4707ee854f4b693bc32f1f6a0651ae84e
p_ash <palliwar.aashay@gmail.com>
2019-08-21 19:28:09 +0530

    Modified flagModel

--- models/flagModel.js
+++ models/flagModel.js
@@ -1,7 +1,7 @@
 // flagRequest(uniqueID) 
 //Function that prompts the server to modify the flag variable.
 
-function flagRequest(uniqueID){
+ var flagRequest = function(uniqueID){
     var branch = uniqueID.substring(0,2);
     var subjectCode = uniqueID.substring(0,7);
     var endpoint = `http://server/studyResouces/branches/${branch}/subjects/${subjectCode}/resources/${uniqueID}`;
