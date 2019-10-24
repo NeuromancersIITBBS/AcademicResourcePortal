dbacf7d0efc26edc725b7e94b2965758d54e6abd
RajShah-1 <srk13@iitbbs.ac.in>
2019-10-24 22:02:23 +0530

    Updated flagModel according to new implementation

--- models/flagModel.js
+++ models/flagModel.js
@@ -1,15 +1,20 @@
 // flagRequest(uniqueID)
 //Function that prompts the server to modify the flag variable.
 
- var flagRequest = function(uniqueID){
+ var flagRequest = function(uniqueID, reason){
+
     let branch = uniqueID.substring(0,2);
     let subjectCode = uniqueID.substring(0,7);
     uniqueID = uniqueID.substring(7,uniqueID.length);
     let endpoint = `https://localhost:3000/studyResouces/${branch}/subjects/${subjectCode}/resources/${uniqueID}`;
+    // If flag is 1 server should increment current count by 1
+    let data = {id: uniqueID, flag: 1, flagReason: reason};
     let response;
     $.ajax({
         url: endpoint,
-        type: 'PUT',
+        method: 'PUT',
+        data: JSON.stringify(data),
+        contentType: "application/json",
         error: function(xhr){
           alert("Something went wrong, please try again.");
         },
